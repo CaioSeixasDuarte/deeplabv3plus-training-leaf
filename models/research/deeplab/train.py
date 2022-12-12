3968eeecc70fcdc6ef2ac6f90470cf03a16c816d
KarlaFlorentino <karlaflorentino1999@gmail.com>
2022-12-12 20:43:10 -0300

    fix errors

--- models/research/deeplab/train.py
+++ models/research/deeplab/train.py
@@ -321,17 +321,17 @@ def main(unused_argv):
 
     # Create the global step on the device storing the variables.
     with tf.device(config.variables_device()):
-      global_step = tf.train.get_or_create_global_step()
+      #global_step = tf.train.get_or_create_global_step()
 
       # Create a variable to hold the global_step.
-      #global_step_tensor = tf.Variable(10, trainable=False, name='global_step')
+      global_step_tensor = tf.Variable(10, trainable=False, name='global_step')
       # Create a session.
       sess = tf.compat.v1.Session()
       # Initialize the variable
-      sess.run(global_step.initializer)
+      sess.run(global_step_tensor.initializer)
       # Get the variable value.
       print('global_step: %s' % tf.compat.v1.train.global_step(sess,
-      global_step))
+      global_step_tensor))
 
 
       # Define the model and create clones.
@@ -430,7 +430,7 @@ def main(unused_argv):
 
       # Create gradient update op.
       grad_updates = optimizer.apply_gradients(
-          grads_and_vars, global_step=global_step)
+          grads_and_vars, global_step=global_step_tensor)
       update_ops.append(grad_updates)
       update_op = tf.group(*update_ops)
       with tf.control_dependencies([update_op]):
