4d9d2163ce16397c4b9f452470da5a6fb828bdf9
KarlaFlorentino <karlaflorentino1999@gmail.com>
2022-12-13 11:34:49 -0300

    fix errors

--- models/research/deeplab/utils/train_utils.py
+++ models/research/deeplab/utils/train_utils.py
@@ -313,7 +313,8 @@ def get_model_learning_rate(learning_policy,
     ValueError: If `boundaries` and `boundary_learning_rates` are not set for
       multi_steps learning rate decay.
   """
-  global_step = tf.train.get_or_create_global_step()
+  #global_step = tf.train.get_or_create_global_step()
+  global_step = tf.Variable(0, trainable=False, name='global_step')
   adjusted_global_step = tf.maximum(global_step - slow_start_step, 0)
   if decay_steps == 0.0:
     tf.logging.info('Setting decay_steps to total training steps.')
